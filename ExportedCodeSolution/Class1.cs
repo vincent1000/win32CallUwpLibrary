7136929b9eeaf0dfc2176a136d6c883a3667c4b5
vincent1000 <41051215+vincent1000@users.noreply.github.com>
2020-06-22 09:30:51 +0800

    Add files via upload

---
+++ ExportedCodeSolution/Class1.cs
@@ -0,0 +1,20 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+namespace ExportedCodeSolution
+{
+    public class Class1
+    {
+        [DllExport(ExportName = "callUri", CallingConvention = CallingConvention.StdCall)]
+        static public async void callUri()
+        {
+            Console.WriteLine("call URI start");
+            await Launcher.LaunchUriAsync(new Uri("www.bing.com"));
+        }
+    }
+}
---
+++ ExportedCodeSolution/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+
+using CallUriFn = void(__stdcall*) ();
+
+int main()
+{
+	HMODULE mod = LoadLibraryA("ExportedCodeSolution.dll");
+	CallUriFn fn = reinterpret_cast<CallUriFn>(GetProcAddress(mod, "callUri"));
+	fn();
+}
